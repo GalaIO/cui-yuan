52e70998ee6775b92a3599012e9bf8ea9acb0e6c
vivian-xu <vivian.zxx53@gmail.com>
2016-01-20 02:43:02 +0800

    layout box for elements in shopin page

--- cui-yuan/FormMain.Designer.cs
+++ cui-yuan/FormMain.Designer.cs
@@ -34,10 +34,25 @@
             this.tabPage_Shopout = new System.Windows.Forms.TabPage();
             this.panel_shopin = new System.Windows.Forms.Panel();
             this.tableLayoutPanel_Shopin = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel_VideoPlayer = new System.Windows.Forms.TableLayoutPanel();
+            this.panel_videoPlayer = new System.Windows.Forms.Panel();
+            this.panel_photoPreview = new System.Windows.Forms.Panel();
+            this.groupBox_videoPlayer = new System.Windows.Forms.GroupBox();
+            this.groupBox_photoPreview = new System.Windows.Forms.GroupBox();
+            this.panel_shopin_detail = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel_detail = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox_weight = new System.Windows.Forms.GroupBox();
+            this.groupBox_notes = new System.Windows.Forms.GroupBox();
             this.panelMain.SuspendLayout();
             this.tabControlMain.SuspendLayout();
             this.tabPage_Shopin.SuspendLayout();
             this.panel_shopin.SuspendLayout();
+            this.tableLayoutPanel_Shopin.SuspendLayout();
+            this.tableLayoutPanel_VideoPlayer.SuspendLayout();
+            this.panel_videoPlayer.SuspendLayout();
+            this.panel_photoPreview.SuspendLayout();
+            this.panel_shopin_detail.SuspendLayout();
+            this.tableLayoutPanel_detail.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMain
@@ -96,6 +111,8 @@
             this.tableLayoutPanel_Shopin.ColumnCount = 2;
             this.tableLayoutPanel_Shopin.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel_Shopin.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel_Shopin.Controls.Add(this.tableLayoutPanel_VideoPlayer, 0, 0);
+            this.tableLayoutPanel_Shopin.Controls.Add(this.panel_shopin_detail, 0, 1);
             this.tableLayoutPanel_Shopin.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel_Shopin.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel_Shopin.Name = "tableLayoutPanel_Shopin";
@@ -104,6 +121,103 @@
             this.tableLayoutPanel_Shopin.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel_Shopin.Size = new System.Drawing.Size(967, 573);
             this.tableLayoutPanel_Shopin.TabIndex = 0;
+            // 
+            // tableLayoutPanel_VideoPlayer
+            // 
+            this.tableLayoutPanel_VideoPlayer.ColumnCount = 2;
+            this.tableLayoutPanel_VideoPlayer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel_VideoPlayer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel_VideoPlayer.Controls.Add(this.panel_videoPlayer, 0, 0);
+            this.tableLayoutPanel_VideoPlayer.Controls.Add(this.panel_photoPreview, 1, 0);
+            this.tableLayoutPanel_VideoPlayer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel_VideoPlayer.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel_VideoPlayer.Name = "tableLayoutPanel_VideoPlayer";
+            this.tableLayoutPanel_VideoPlayer.RowCount = 1;
+            this.tableLayoutPanel_VideoPlayer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel_VideoPlayer.Size = new System.Drawing.Size(861, 400);
+            this.tableLayoutPanel_VideoPlayer.TabIndex = 0;
+            // 
+            // panel_videoPlayer
+            // 
+            this.panel_videoPlayer.Controls.Add(this.groupBox_videoPlayer);
+            this.panel_videoPlayer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_videoPlayer.Location = new System.Drawing.Point(3, 3);
+            this.panel_videoPlayer.Name = "panel_videoPlayer";
+            this.panel_videoPlayer.Size = new System.Drawing.Size(424, 394);
+            this.panel_videoPlayer.TabIndex = 0;
+            // 
+            // panel_photoPreview
+            // 
+            this.panel_photoPreview.Controls.Add(this.groupBox_photoPreview);
+            this.panel_photoPreview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_photoPreview.Location = new System.Drawing.Point(433, 3);
+            this.panel_photoPreview.Name = "panel_photoPreview";
+            this.panel_photoPreview.Size = new System.Drawing.Size(425, 394);
+            this.panel_photoPreview.TabIndex = 1;
+            // 
+            // groupBox_videoPlayer
+            // 
+            this.groupBox_videoPlayer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox_videoPlayer.Location = new System.Drawing.Point(0, 0);
+            this.groupBox_videoPlayer.Name = "groupBox_videoPlayer";
+            this.groupBox_videoPlayer.Size = new System.Drawing.Size(424, 394);
+            this.groupBox_videoPlayer.TabIndex = 0;
+            this.groupBox_videoPlayer.TabStop = false;
+            this.groupBox_videoPlayer.Text = "拍照摄像头";
+            // 
+            // groupBox_photoPreview
+            // 
+            this.groupBox_photoPreview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox_photoPreview.Location = new System.Drawing.Point(0, 0);
+            this.groupBox_photoPreview.Name = "groupBox_photoPreview";
+            this.groupBox_photoPreview.Size = new System.Drawing.Size(425, 394);
+            this.groupBox_photoPreview.TabIndex = 0;
+            this.groupBox_photoPreview.TabStop = false;
+            this.groupBox_photoPreview.Text = "照片预览";
+            // 
+            // panel_shopin_detail
+            // 
+            this.panel_shopin_detail.Controls.Add(this.tableLayoutPanel_detail);
+            this.panel_shopin_detail.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_shopin_detail.Location = new System.Drawing.Point(3, 409);
+            this.panel_shopin_detail.Name = "panel_shopin_detail";
+            this.panel_shopin_detail.Size = new System.Drawing.Size(861, 161);
+            this.panel_shopin_detail.TabIndex = 1;
+            // 
+            // tableLayoutPanel_detail
+            // 
+            this.tableLayoutPanel_detail.ColumnCount = 2;
+            this.tableLayoutPanel_detail.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel_detail.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel_detail.Controls.Add(this.groupBox_weight, 0, 0);
+            this.tableLayoutPanel_detail.Controls.Add(this.groupBox_notes, 1, 0);
+            this.tableLayoutPanel_detail.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel_detail.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel_detail.Name = "tableLayoutPanel_detail";
+            this.tableLayoutPanel_detail.RowCount = 1;
+            this.tableLayoutPanel_detail.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel_detail.Size = new System.Drawing.Size(861, 161);
+            this.tableLayoutPanel_detail.TabIndex = 0;
+            // 
+            // groupBox_weight
+            // 
+            this.groupBox_weight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox_weight.Location = new System.Drawing.Point(3, 3);
+            this.groupBox_weight.Name = "groupBox_weight";
+            this.groupBox_weight.Size = new System.Drawing.Size(424, 155);
+            this.groupBox_weight.TabIndex = 0;
+            this.groupBox_weight.TabStop = false;
+            this.groupBox_weight.Text = "称重";
+            // 
+            // groupBox_notes
+            // 
+            this.groupBox_notes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox_notes.Location = new System.Drawing.Point(433, 3);
+            this.groupBox_notes.Name = "groupBox_notes";
+            this.groupBox_notes.Size = new System.Drawing.Size(425, 155);
+            this.groupBox_notes.TabIndex = 1;
+            this.groupBox_notes.TabStop = false;
+            this.groupBox_notes.Text = "备注";
             // 
             // FormMain
             // 
@@ -117,6 +231,12 @@
             this.tabControlMain.ResumeLayout(false);
             this.tabPage_Shopin.ResumeLayout(false);
             this.panel_shopin.ResumeLayout(false);
+            this.tableLayoutPanel_Shopin.ResumeLayout(false);
+            this.tableLayoutPanel_VideoPlayer.ResumeLayout(false);
+            this.panel_videoPlayer.ResumeLayout(false);
+            this.panel_photoPreview.ResumeLayout(false);
+            this.panel_shopin_detail.ResumeLayout(false);
+            this.tableLayoutPanel_detail.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -129,6 +249,15 @@
         private System.Windows.Forms.TabPage tabPage_Shopout;
         private System.Windows.Forms.Panel panel_shopin;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_Shopin;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_VideoPlayer;
+        private System.Windows.Forms.Panel panel_videoPlayer;
+        private System.Windows.Forms.Panel panel_photoPreview;
+        private System.Windows.Forms.GroupBox groupBox_videoPlayer;
+        private System.Windows.Forms.GroupBox groupBox_photoPreview;
+        private System.Windows.Forms.Panel panel_shopin_detail;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_detail;
+        private System.Windows.Forms.GroupBox groupBox_weight;
+        private System.Windows.Forms.GroupBox groupBox_notes;
     }
 }
 
