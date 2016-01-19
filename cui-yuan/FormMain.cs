4fe3f4f8039e26e90b244f9632f4250cceded8d6
vivian-xu <vivian.zxx53@gmail.com>
2016-01-20 03:20:18 +0800

    init cam at startup

--- cui-yuan/FormMain.cs
+++ cui-yuan/FormMain.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using AForge.Video.DirectShow;
+
 namespace cui_yuan
 {
     public partial class FormMain : Form
@@ -14,6 +16,21 @@ namespace cui_yuan
         public FormMain()
         {
             InitializeComponent();
+            InitCam();
+        }
+
+        private void InitCam()
+        {
+            comboBox_videoPlayer.DataSource = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            comboBox_videoPlayer.DisplayMember = "name";
+            comboBox_videoPlayer.ValueMember = "MonikerString";
+
+            if (comboBox_videoPlayer.Items.Count == 0)
+            {
+                MessageBox.Show("没有找到摄像头. 请插好摄像头并重启软件");
+            }
+
+            comboBox_videoPlayer.SelectedIndex = 0;
         }
     }
 }
